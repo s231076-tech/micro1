784b040d5abedfbc632968edb02a6705f363c0da
s231076-tech <s231076@student.dgs.edu.hk>
2026-01-14 14:24:51 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+Sugar.onButtonEvent(DigitalPin.P0, function () {
+    Sugar.ledOnoff(DigitalPin.P1, Sugar.LEDSta.On)
 })
+input.onButtonPressed(Button.A, function () {
+    Sugar.ledOnoff(DigitalPin.P15, Sugar.LEDSta.Off)
+})
+Sugar.onButtonEvent(DigitalPin.P14, function () {
+    Sugar.ledOnoff(DigitalPin.P15, Sugar.LEDSta.On)
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
