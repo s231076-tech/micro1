8275b9d33606f1627712ca4a9ed8e63d7ef08f0c
s231076-tech <s231076@student.dgs.edu.hk>
2026-01-14 14:26:27 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ Sugar.onButtonEvent(DigitalPin.P0, function () {
 })
 input.onButtonPressed(Button.A, function () {
     Sugar.ledOnoff(DigitalPin.P15, Sugar.LEDSta.Off)
+    basic.showIcon(IconNames.Heart)
 })
 Sugar.onButtonEvent(DigitalPin.P14, function () {
     Sugar.ledOnoff(DigitalPin.P15, Sugar.LEDSta.On)
